7b72c2f21e7fc7cd2ae782d3bda086f8c2adeaa0
Caspar Lant <casparlant@gmail.com>
2015-02-19 12:35:33 -0500

    Caspar

--- 2-DirectoryTree/FileOnDisk.java
+++ 2-DirectoryTree/FileOnDisk.java
@@ -1,12 +1,25 @@
 public class FileOnDisk implements Comparable<FileOnDisk>{
 
+    private int size = 0;
     public FileOnDisk(){
     }
 
     public int compareTo(FileOnDisk other){
-        if (this == other)
-            return 
         return -1;
     }
 
+    public Boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+        if (
+
+
+        return true;
+
+
+    }
 }
