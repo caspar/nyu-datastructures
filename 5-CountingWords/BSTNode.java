a41cf3378b25ffc0ba78310043d2ca17f9049a15
Caspar Lant <casparlant@gmail.com>
2015-04-20 16:03:26 -0400

    Caspar

---
+++ 5-CountingWords/BSTNode.java
@@ -0,0 +1,50 @@
+/**
+ * @author Caspar Lant
+ *
+ */
+public class BSTNode<E extends Comparable<E>>{
+
+    private E data = null;
+    private BSTNode<E> left  = null;
+    private BSTNode<E> right = null;
+
+    public Node(E data){
+        this.data = data;
+    }
+
+    public Node(){
+    }
+
+    public int compareTo(BSTNode<E> node){
+        if (node.getData() > data)
+            return -1;
+        if (node.getData() < data)
+            return 1;
+        return 0;
+    }
+
+    public BSTNode<E> getLeft(){
+        return left;
+    }
+
+    public BSTNode<E> getRight(){
+        return right;
+    }
+
+    public void setLeft(BSTNode<E> left){
+        this.left = left;
+    }
+
+    public void setRight(BSTNode<E> right){
+        this.right = right;
+    }
+
+    public E getData(){
+        return data;
+    }
+
+    public void setData(E data){
+        this.data = data;
+    }
+
+}
---
+++ 5-CountingWords/BinarySearchTree.java
@@ -0,0 +1,36 @@
+class BinarySearchTree{
+
+    private BSTNode<E> root;
+
+    public void sort(){
+
+    }
+
+    public int size(){
+        return size(root);
+    }
+
+    public int size(BSTNode<E> node){
+        if (node = null)
+            return 0;
+        return 1 + size(node.getRight()) + size(node.getLeft());
+    }
+
+    public int search(array, key, min, max){
+        if (max < min)
+            return -1;
+        int mid = (max + min) / 2;
+        if (array[mid] == key)
+            return mid;
+        if (array[mid] < key)
+            return search(array, key, min-1, max);i
+
+    }
+    public void add(E data){
+        ///////// //////// //////// //////// //////// ////////
+        ///       //    // //       //    // //    // //    //
+        ///       //////// //////// //////// //////// ////////
+        ///       //    //       // //       //    // //  //
+        ///////// //    // //////// //       //    // //   //
+    }
+}
